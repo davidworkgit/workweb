263bfcb3d4dde2a12ad932e4b536274b780be7d1
David <newhwdy@gmail>
2020-01-04 18:33:57 +0900

    update

--- src/app/components/updownload/updown-top.component.ts
+++ src/app/components/updownload/updown-top.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { asEnumerable } from 'linq-es2015';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'why-updown-top',
@@ -8,16 +9,28 @@ import { asEnumerable } from 'linq-es2015';
 })
 export class UpdownTopComponent implements OnInit {
   count: any;
-  constructor() { }
+  selectedFile: File;
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   
   }
 
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0]
+  }
+
+  onUpload() {
+    // this.http is the injected HttpClient
+    const uploadData = new FormData();
+    uploadData.append('myFile', this.selectedFile, this.selectedFile.name);
+    this.http.post('gs://fir-demo-project.appspot.com', uploadData)
+      .subscribe(res => console.log(res));
+  }
   testclick(){
    
     this.count = asEnumerable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).Where(a => a % 2 == 0).ToArray();
     console.log(this.count);
   }
-
+  
 }
